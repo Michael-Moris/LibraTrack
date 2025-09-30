f7527755ecd6d5bfdb7ea40a513ec16ea75b0ce9
Michael-Moris <michaelmorisdev@gmail.com>
2025-09-30 06:20:59 +0300

    Add data manipulation examples in Program.cs
    Introduces a new "Data Manipulation" region with two subsections:
    1. Retrieve book titles, category titles, and author names for books priced over 300.
    2. Retrieve authors and their books if they exist.
    
    Both sections include commented-out code demonstrating database queries using Entity Framework.

--- LibraTrack/Program.cs
+++ LibraTrack/Program.cs
@@ -18,6 +18,50 @@ namespace LibraTrack
             //Console.WriteLine(Result ? "Data Seeded Successfully!..." : "No Data Seeded!...");
             #endregion
 
+            #region Data Manipulation
+
+            #region Try 01: Retrieve the book title, its category title , and the author’s full name for all books whose price is greater than 300.
+            //var books = dbContext.Books
+            //    .Where(b => b.Price > 300)
+            //    .Select(b => new
+            //    {
+            //        b.Title,
+            //        CategoryTitle = b.Category.Title,
+            //        AuthorFullName = b.Author.FirstName + " " + b.Author.LastName,
+            //        price = b.Price
+            //    })
+            //    .ToList();
+
+            //Console.WriteLine("Books with Price Greater than 300: \n");
+            //foreach (var book in books)
+            //{
+            //    Console.WriteLine($"Title: {book.Title}, Category: {book.CategoryTitle}, Author: {book.AuthorFullName}, Price: {book.price}");
+            //}
+            #endregion
+
+            #region Try 02: Retrieve All Authors And His/Her Books if Exists.
+            //var AuthorsBooks = dbContext.Authors
+            //    .Select(a => new
+            //    {
+            //        AuthorFullName = a.FirstName + " " + a.LastName,
+            //        Books = a.Books.Select(b => b.Title).ToList()
+            //    }).ToList();
+
+            //foreach (var author in AuthorsBooks)
+            //{
+            //    Console.WriteLine($"Author: {author.AuthorFullName}");
+            //    if (author.Books.Any())
+            //    {
+            //        foreach (var book in author.Books)
+            //            Console.WriteLine($"\tBook: {book}.");
+            //    }
+            //    else
+            //        Console.WriteLine("\tNo Books Found.");
+            //}
+            #endregion
+
+            #endregion
+
         }
     }
 }
